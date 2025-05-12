56fb20ec1cc44d66cc96437ce3687729078ac963
Edward Junsyoung Lee <junsyounged.lee@mail.utoronto.ca>
2025-05-12 02:23:29 -0400

    generating

--- zcurve.R
+++ zcurve.R
@@ -1,4 +1,29 @@
 library(zcurve)
+library(faux)
+library(truncnorm)
 set.seed(666)
 
-#test commit
+fit <- zcurve(OSC.z)
+
+zcurve_plot <- plot(
+  fit,CI=TRUE,annotation=TRUE,main="OSC 2015")
+
+#N(0,1)
+#n=15,000 simulates studies
+#each has n=40, half in control and half in exp group
+
+n <- 15000
+z_scores <- numeric(n)
+
+
+for (i in 1:15000) {
+  control_group <- rnorm(n=20,mean=0,sd=1)
+  exp_group <- rnorm(n=20,mean=0,sd=1)
+  result <- t.test(control_group, exp_group)
+  p_value <- result$p.value
+  z_scores[i] <- p_value
+}
+
+
+
+
