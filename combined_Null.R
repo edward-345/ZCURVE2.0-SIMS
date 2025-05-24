93f0bc6c7bc1e95531069989e3424f0714567cd9
Edward Junsyoung Lee <junsyounged.lee@mail.utoronto.ca>
2025-05-23 17:54:05 -0700

    Combined Situation A+B

---
+++ combined_Null.R
@@ -0,0 +1,89 @@
+#Situations A and B combined
+zscores_X <- numeric(15000)
+X <- 1 
+
+pvalues_scenarioX <- numeric(15000)
+
+for (i in 1:15000) {
+  control_X <- rnorm_multi(
+    n = 10, vars = 2, mu = c(0,0),sd = c(1,1), r = 0.5,
+    varnames = c("Control1","Control2"))
+  exp_X <- rnorm_multi(
+    n = 10, vars = 2, mu = c(0,0), sd = c(1,1), r = 0.5,
+    varnames = c("Dependent1","Dependent2"))
+  
+  result1 <- t.test(control_X$Control1, exp_X$Dependent1,
+                    var.equal = TRUE)
+  pvalue1 <- result1$p.value
+  result2 <- t.test(control_X$Control2, exp_X$Dependent2,
+                    var.equal = TRUE)
+  pvalue2 <- result2$p.value
+  result3 <- t.test(rowMeans(cbind(control_X$Control1, control_X$Control2)),
+                    rowMeans(cbind(exp_X$Dependent1, exp_X$Dependent2)),
+                    var.equal = TRUE)
+  pvalue3 <- result3$p.value
+  
+  pvalues_X <- c(pvalue1, pvalue2, pvalue3)
+  
+  if (min(pvalues_X) <= 0.05) {
+    pvalues_scenarioX[i] <- min(pvalues_X)
+    zvalue_X <- abs(qnorm(min(pvalues_X)/2,
+                          lower.tail = FALSE))
+    zscores_X[X] <- zvalue_X
+    X <- X + 1
+  } else {
+    extracontrol_X <- rnorm_multi(
+      n = 10, vars = 2, mu = c(0,0),sd = c(1,1), r = 0.5,
+      varnames = c("ExCon1","ExCon2"))
+    extraexp_X <- rnorm_multi(
+      n = 10, vars = 2, mu = c(0,0),sd = c(1,1), r = 0.5,
+      varnames = c("ExDep1","ExDep2"))
+    
+    combined_Control1 <- c(control_X$Control1, extracontrol_X$ExCon1)
+    combined_Dep1 <- c(exp_X$Dependent1, extraexp_X$ExDep1)
+    combined_Control2 <- c(control_X$Control2, extracontrol_X$ExCon2)
+    combined_Dep2 <- c(exp_X$Dependent2, extraexp_X$ExDep2) 
+    
+    combined_control_avg <- rowMeans(
+      cbind(combined_Control1, combined_Control2))
+    combined_exp_avg     <- rowMeans(cbind(combined_Dep1, combined_Dep2))
+    
+    ExtraResult1 <- t.test(combined_Control1,
+                           combined_Dep1, var.equal = TRUE)
+    ExtraPvalue1 <- ExtraResult1$p.value
+    
+    ExtraResult2 <- t.test(combined_Control2,
+                           combined_Dep2, var.equal = TRUE)
+    ExtraPvalue2 <- ExtraResult2$p.value
+    
+    ExtraResult3 <- t.test(combined_control_avg,
+                           combined_exp_avg, var.equal = TRUE)
+    ExtraPvalue3 <- ExtraResult3$p.value
+    
+    ExtraPvalues_X <- c(ExtraPvalue1, ExtraPvalue2, ExtraPvalue3)
+    
+    if (min(ExtraPvalues_X) <= 0.05) {
+      pvalues_scenarioX[i] <- min(ExtraPvalues_X)
+      extrazvalue_X <- abs(qnorm(min(ExtraPvalues_X)/2, lower.tail = FALSE))
+      zscores_X[X] <- extrazvalue_X
+      X <- X+1
+    } else {
+      pvalues_scenarioX[i] <- min(ExtraPvalues_X)
+    }
+  }
+}
+
+
+zscores_X <- zscores_X[1:(X - 1)]
+
+#Note that the proportion of p-values align with Simmons et al., 2011
+pvalues1_X <- pvalues_scenarioX[pvalues_scenarioX < 0.1]
+prop1_X <- (length(pvalues1_X)/15000)*100
+
+pvalues05_X <- pvalues_scenarioX[pvalues_scenarioX < 0.05]
+prop05_X <- (length(pvalues05_X)/15000)*100
+
+pvalues01_X <- pvalues_scenarioX[pvalues_scenarioX < 0.01]
+prop01_X <- (length(pvalues01_X)/15000)*100
+
+proportions_X <- c(prop1_X, prop05_X, prop01_X)
