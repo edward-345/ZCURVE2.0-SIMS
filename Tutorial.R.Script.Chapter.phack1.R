95013ba7b6a222754de28483ae8a62eb28dc4ed7
Edward Junsyoung Lee <junsyounged.lee@mail.utoronto.ca>
2025-08-25 01:07:14 -0400

    Sample generator

--- Tutorial.R.Script.Chapter.phack1.R
+++ Tutorial.R.Script.Chapter.phack1.R
@@ -31,44 +31,53 @@ source(zcurve3b)
 pcurve <- "https://github.com/UlrichSchimmack/zcurve3.0/raw/refs/heads/main/Pcurve.Function.R"
 source(pcurve)
 
+#T-Tests for multiple covariates
+multi_ttests <- function(control, exp) {
+  
+  ttest_res <- list()
+  
+  for (i in seq_len(ncol(control))) {
+    nm <- paste0("dv", i, "_ttest")
+    ttest_res[[nm]] <- t.test(control[, i], exp[, i], var.equal = TRUE)
+  }
+  ttest_res$avg_ttest <- t.test(rowMeans(control), rowMeans(exp_grp),
+                                var.equal = TRUE)
+  return(ttest_res)
+  }
 
+################################################################################
 ### 1.0 – ILLUSTRATION WITH EXAMPLES
 
 
-
-
-
-
-
-
-
-
-
-
+################################################################################
 ### 2.0 – SIMULATION 
 
 sim.runs <- 10                 # repeat each simulation 100 times
 sim.k.sig <- 1000             # how many significant results?
 
-sim.es.mean <- seq(0,1,.2)    # mean effect size, Cohen's d 6 elements
+sim.es.mean <- seq(0,1,.2)    # mean effect size, Cohen's d
+sim.n.obs <- c(10, 20, 40, 60, 80, 100)   # sample size per cell
+sim.r.var <- seq(0,1,.2)      # CORRELATION R BETWEEN COVARIATES
+sim.n.vars <- c(2, 4, 6, 8, 10, 12) # NUMBER OF COVARIATES 
 #sim.es.sd <- seq(0,.6,.2) # heterogeneity of es (tau) 
-sim.n.obs <- c(10, 20, 40, 60, 80, 100)   # sample size per cell 5 elements
-sim.r.var <- seq(0,1,.2)      # correlation r between multiple vars 6 elements
 
-# create the simulation conditions
-
+# MATRIX OF ALL POSSIBLE SIMULATION CONDITION COMBINATIONS 1296 X 5
 sims <- c()
-sim.i = 0
+sim.i <- 0
 for (i in 1:length(sim.es.mean) ) {
 	for (j in 1:length(sim.r.var)) {
 		for (k in 1:length(sim.n.obs) ) {
+		  for (l in 1:length(sim.n.vars) ) {
 			sim.i = sim.i + 1
-			sims = rbind(sims,c(sim.i,sim.es.mean[i],sim.r.var[j],sim.n.obs[k]) )
-}}}
-sims = data.frame(sims)
-colnames(sims) = c("run","es.mean","r.var","n.obs")
+			sims = rbind(sims,c(sim.i,
+			                    sim.es.mean[i],
+			                    sim.r.var[j],
+			                    sim.n.obs[k],
+			                    sim.n.vars[l]) )
+}}}}
+sims <- data.frame(sims)
+colnames(sims) <- c("run","es.mean","r.var","n.obs", "n.vars")
 dim(sims)
-# sims IS A DATA FRAME CONTAINING ALL COMBINATIONS OF STUDY CONDITIONS
 
 run.i <- 66       # used for testing without loop
 b <- 1            # form row b
@@ -81,189 +90,178 @@ res = c()        # collect the results
 ################################################################################
 # CURRENTLY for(66 in 1:5) {}
 for (run.i in b:e ) { # for loop to run the simulations
-
-#Run iteration tracker
-print(paste("Run: ",run.i))
-sims[run.i,]
-
-# Vector of sample1 is 1:15, sample2 is 16:30, etc
-sample1 <- 1:sims$n.obs[run.i]
-sample2 <- (sims$n.obs[run.i]+1):(2*sims$n.obs[run.i])
-sample3 <- (2*sims$n.obs[run.i]+1):(3*sims$n.obs[run.i])
-samples <- list(sample1 = sample1,
-                    sample2 = sample2,
-                    sample3 = sample3,
-                    sample12 = c(sample1, sample2),
-                    sample13 = c(sample1, sample3),
-                    sample23 = c(sample2, sample3),
-                    sample123 = c(sample1, sample2, sample3))
-
-results <- c()
-count.sig <- 0
-count.run <- 0
-
-#set seed every run?
-set.seed(run.i)
-
-while (count.sig < sim.k.sig) {
-
-count.run = count.run + 1
-
-pop.es = rnorm(1,sims$es.mean[run.i],sims$es.sd[run.i]);pop.es
-exp_group <- rnorm(n = 3*sims$n.obs[run.i], mean = pop.es, sd = 1)
-summary(exp_group)
-
-control <- rnorm(n = 3*sims$n.obs[run.i], mean = 0, sd = 1)
-summary(control)
-
-i.patch = 1   
-res.run = c()
-for (i.patch in 1:7) {
-      sub_exp <- exp_group[unlist(samples[i.patch])]
-      sub_control <- control[unlist(samples[i.patch])] 
-      length(sub_exp)
-      length(sub_control)
-      tval <- t.test(sub_exp, sub_control, paired=FALSE,var.equal=TRUE)$statistic
-      dfs <- t.test(sub_exp, sub_control, var.equal=TRUE)$parameter
-      pval <- t.test(sub_exp, sub_control, var.equal=TRUE)$p.value
-      se <- t.test(sub_exp, sub_control, var.equal=TRUE)$stderr
-      es <- t.test(sub_exp, sub_control, var.equal=TRUE)$estimate
-      res.run = rbind(res.run,c(count.run,pop.es,es[1]-es[2],se,tval,dfs,pval)	)
-    }
-
-res.run
-
-results = rbind(results,res.run)
-
-if (min(res.run[,7]) < .05) count.sig = count.sig + 1
-
-print(count.run)
-print(count.sig)
-
-
-} # EOF while loop 
-
-count.run
-count.sig
-
-dim(results)
-
-results = data.frame(results)
-results$N = results$df+2
-results$se = 2/sqrt(results$N)
-results$nct = abs(results[,2]/results$se)  
-results$abs.t = abs(results$t)
-results$z = qnorm(pt(results$abs.t,results$N-2,log.p=TRUE),log.p=TRUE)
-results$pow.dir = pt(qt(.975,results$df),results$df,results$nct,lower.tail=FALSE)
-results$pow.sign.err = pt(-qt(.975,results$df),results$df,results$nct,lower.tail=TRUE)
-results$pow = results$pow.dir + results$pow.sign.err
-
-summary(results)
-
-plot(results$abs.t,results$z)
-cor(results$abs.t,results$z)
-
-col1b = rgb(0, 100, 100, 255, max = 255,alpha = 50)
-col2b = rgb(100,0,100, max = 255, alpha = 50)
-
-cor(results$N,results$pow)
-cor(abs(results[,2]),results$pow) #  only if effect sizes are heterogeneous
-
-true.edr = mean(results$pow);true.edr
-true.err = sum(results$pow*results$pow.dir)/sum(results$pow);true.err
-
-tab = table(results[,7] < .05);tab/sum(tab) #ODR
-
-true.edr
-true.err
-
-sim.z.all = results$z
-sim.t.all = results$abs.t
-
-
-sim.min.p = apply(matrix(results$V7,7,),2,function(x) as.numeric(x == min(x)))
-results$min.p = c(sim.min.p)
-table(results$min.p)
-table(sim.z.all[results$min.p == TRUE] > 1.96)
-
-sim.z.max = sim.z.all[sim.min.p == TRUE]
-sim.t.max = sim.t.all[sim.min.p == TRUE]
-sim.df.max = results$df[sim.min.p == TRUE]
-
-
-source(zcurve3)
-Title = paste(round(true.edr*100),"  ",round(true.err*100))
-res.1 = Zing(sim.z.all);res.1
-res.1 = res.1$res[2:3];res.1
-
-source(zcurve3)
-Title = paste(round(true.edr*100),"  ",round(true.err*100))
-res.2 = Zing(sim.t.all);res.2
-res.2 = res.2$res[2:3];res.2
-
-source(zcurve3)
-Title = paste(round(true.edr*100),"  ",round(true.err*100))
-Est.Method = "DF"
-res.3 = Zing(sim.t.all,df=median(results$df));res.3
-res.3 = res.3$res[2:3];res.3
-
-source(zcurve3)
-Title = paste(round(true.edr*100),"  ",round(true.err*100))
-res.4 = Zing(sim.z.max);res.4
-res.4 = res.4$res[2:3];res.4
-
-source(zcurve3)
-Title = paste(round(true.edr*100),"  ",round(true.err*100))
-TEST4BIAS = TRUE
-just = 1
-Int.Beg = 2 + just
-res.5 = Zing(sim.z.max);res.5
-res.5 = res.5$res[2:3];res.5
-
-source(zcurve3)
-Title = paste(round(true.edr*100),"  ",round(true.err*100))
-TEST4BIAS = TRUE
-just = 1
-Int.Beg = 2 + just
-res.6 = Zing(sim.t.max,median(sim.df.max));res.6
-res.6 = res.6$res[2:3];res.6
-
-pcurve.input = paste0("t(",results$df,") = ",sim.t.all)
-res.pcurve.1 = pcurve_app(pcurve.input,SHOW.PLOT=TRUE)
-res.pcurve.1 
-if (res.pcurve.1[2] > res.pcurve.1[1]) res.pcurve.1[1] = res.pcurve.1[2]
-
-
-pcurve.input = paste0("t(",sim.df.max,") = ",sim.t.max)
-res.pcurve.2 = pcurve_app(pcurve.input,SHOW.PLOT=TRUE)
-res.pcurve.2 
-true.err
-if (res.pcurve.2[2] > res.pcurve.2[1]) res.pcurve.2[1] = res.pcurve.2[2]
-
-res.run = rbind(
-	res.1,
-	res.2,
-	res.3,
-	res.4,
-	res.5,
-	res.6,
-	c(res.pcurve.1[1],NA),
-	c(res.pcurve.2[1],NA)
-)
-
-rownames(res.run) = c("z.all","t.all","t.curve.all","z.sig","z.sig.sel",
-	"t.sig.sel","pcurve.all","pcurve.sig")
-
-round(rbind(c(true.err,true.edr),res.run),2)
-
-res.run = c(unlist(sims[run.i,]),true.err,res.run[,1],true.edr,res.run[,2])
-print(res.run)
-
-res = rbind(res,res.run)
-
-write.table(res,"sim.phack1.dat")    # write results each trial
-                                   # can resume if stopped 
-                                   # by loading the completed results
+  
+  #Run iteration tracker
+  print(paste("Run: ",run.i))
+  sims[run.i,]
+  
+  #EMPTY LIST OF TTEST SUMMARIES FROM SIMULATIONS
+  results <- data.frame()
+  #SIGNIFCANT RESULT COUNTER
+  count.sig <- 0
+  #NUMBER OF RUNS COUNTER
+  count.run <- 0
+  
+  #WHILE LOOP GENERATING SAMPLES AND DATA
+  while (count.sig < sim.k.sig) { 
+    
+    #LOOP ITERATION COUNTER
+    count.run <- count.run + 1
+    
+    #GENERATE EXP AND CONTROL SAMPLES AND THEIR Y VALUES
+    exp_group <- rnorm_multi(n = sims$n.obs[run.i],
+                             vars = sims$es.mean[run.i],
+                             sd = 1,
+                             r = sim$r.var[run.i])
+    control_group <- rnorm_multi(n = sims$n.obs[run.i],
+                                 vars = rep(0,),
+                                 sd = 1,
+                                 r = 0)
+    
+    #T-TESTS OF EACH COVARIATE AND THE AVERAGE OF ALL COVARIATES
+    ttest_res <- multi_ttests(exp_group, control_group)
+    
+    #EXTRACT PVALUES FOR COMPARISON
+    pvalues <- sapply(ttest_res, function(x) x$p.value)
+    
+    #EXTRACT VALUES FROM THE MOST SIG TEST FOR RESULTS DATA FRAME
+    i_min <- which.min(vapply(ttest_res, function(m) m$p.value, numeric(1)))
+    sig_ttest <- ttest_res[[i_min]]
+    
+    #MINIMUM PVALUE OF THE T-TESTS
+    min_pvalue <- min(pvalues)
+    
+    tval <- sig_ttest$statistic
+    dfs <- sig_ttest$parameter
+    se <- sig_ttest$stderr
+    es <- sig_ttest$estimate
+    delta <- es[1]-es[2]
+    
+    res.run = rbind(res.run,
+                    c(count.run, pop.es, delta, se, tval, dfs, min_pval))
+    
+    #IF THE LOWEST PVALUE IS SIGNIFCANT ADD TO SIGNIFCANT PVAL COUNTER
+    if (min_pvalue < .05) {
+      count.sig = count.sig + 1
+      }
+    
+  } #END OF WHILE LOOP  
+  
+  results$N <- results$df+2
+  results$se <- 2/sqrt(results$N)
+  results$nct <- abs(results[,2]/results$se)  
+  results$abs.t <- abs(results$t)
+  results$z <- qnorm(pt(results$abs.t,results$N-2,log.p=TRUE),log.p=TRUE)
+  results$pow.dir <- pt(qt(.975,results$df),results$df,results$nct,lower.tail=FALSE)
+  results$pow.sign.err <- pt(-qt(.975,results$df),results$df,results$nct,lower.tail=TRUE)
+  results$pow <- results$pow.dir + results$pow.sign.err
+  
+  summary(results)
+  
+  plot(results$abs.t,results$z)
+  cor(results$abs.t,results$z)
+  
+  col1b = rgb(0, 100, 100, 255, max = 255,alpha = 50)
+  col2b = rgb(100,0,100, max = 255, alpha = 50)
+  
+  cor(results$N,results$pow)
+  cor(abs(results[,2]),results$pow) #  only if effect sizes are heterogeneous
+  
+  true.edr = mean(results$pow);true.edr
+  true.err = sum(results$pow*results$pow.dir)/sum(results$pow);true.err
+  
+  tab = table(results[,7] < .05);tab/sum(tab) #ODR
+  
+  true.edr
+  true.err
+  
+  sim.z.all = results$z
+  sim.t.all = results$abs.t
+  
+  
+  sim.min.p = apply(matrix(results$V7,7,),2,function(x) as.numeric(x == min(x)))
+  results$min.p = c(sim.min.p)
+  table(results$min.p)
+  table(sim.z.all[results$min.p == TRUE] > 1.96)
+  
+  sim.z.max = sim.z.all[sim.min.p == TRUE]
+  sim.t.max = sim.t.all[sim.min.p == TRUE]
+  sim.df.max = results$df[sim.min.p == TRUE]
+  
+  
+  source(zcurve3)
+  Title = paste(round(true.edr*100),"  ",round(true.err*100))
+  res.1 = Zing(sim.z.all);res.1
+  res.1 = res.1$res[2:3];res.1
+  
+  source(zcurve3)
+  Title = paste(round(true.edr*100),"  ",round(true.err*100))
+  res.2 = Zing(sim.t.all);res.2
+  res.2 = res.2$res[2:3];res.2
+  
+  source(zcurve3)
+  Title = paste(round(true.edr*100),"  ",round(true.err*100))
+  Est.Method = "DF"
+  res.3 = Zing(sim.t.all,df=median(results$df));res.3
+  res.3 = res.3$res[2:3];res.3
+  
+  source(zcurve3)
+  Title = paste(round(true.edr*100),"  ",round(true.err*100))
+  res.4 = Zing(sim.z.max);res.4
+  res.4 = res.4$res[2:3];res.4
+  
+  source(zcurve3)
+  Title = paste(round(true.edr*100),"  ",round(true.err*100))
+  TEST4BIAS = TRUE
+  just = 1
+  Int.Beg = 2 + just
+  res.5 = Zing(sim.z.max);res.5
+  res.5 = res.5$res[2:3];res.5
+  
+  source(zcurve3)
+  Title = paste(round(true.edr*100),"  ",round(true.err*100))
+  TEST4BIAS = TRUE
+  just = 1
+  Int.Beg = 2 + just
+  res.6 = Zing(sim.t.max,median(sim.df.max));res.6
+  res.6 = res.6$res[2:3];res.6
+  
+  pcurve.input = paste0("t(",results$df,") = ",sim.t.all)
+  res.pcurve.1 = pcurve_app(pcurve.input,SHOW.PLOT=TRUE)
+  res.pcurve.1 
+  if (res.pcurve.1[2] > res.pcurve.1[1]) res.pcurve.1[1] = res.pcurve.1[2]
+  
+  
+  pcurve.input = paste0("t(",sim.df.max,") = ",sim.t.max)
+  res.pcurve.2 = pcurve_app(pcurve.input,SHOW.PLOT=TRUE)
+  res.pcurve.2 
+  true.err
+  if (res.pcurve.2[2] > res.pcurve.2[1]) res.pcurve.2[1] = res.pcurve.2[2]
+  
+  res.run = rbind(
+    res.1,
+    res.2,
+    res.3,
+    res.4,
+    res.5,
+    res.6,
+    c(res.pcurve.1[1],NA),
+    c(res.pcurve.2[1],NA)
+  )
+  
+  rownames(res.run) = c("z.all","t.all","t.curve.all","z.sig","z.sig.sel",
+                        "t.sig.sel","pcurve.all","pcurve.sig")
+  
+  round(rbind(c(true.err,true.edr),res.run),2)
+  
+  res.run = c(unlist(sims[run.i,]),true.err,res.run[,1],true.edr,res.run[,2])
+  print(res.run)
+  
+  res = rbind(res,res.run)
+  
+  write.table(res,"sim.phack1.dat")    # write results each trial
+  # can resume if stopped 
+  # by loading the completed results
 
 } # End of for loop
 
